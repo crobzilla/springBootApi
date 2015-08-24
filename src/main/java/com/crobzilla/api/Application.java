038a6bd680cae68fff01b1fa957680886879ecef
Chris Robinson <crobinson@mshare.net>
2015-08-24 14:58:50 -0600

    removing unnecessary annotations

--- src/main/java/com/crobzilla/api/Application.java
+++ src/main/java/com/crobzilla/api/Application.java
@@ -5,12 +5,8 @@ import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@ComponentScan
 @SpringBootApplication
-@EnableMongoRepositories
 public class Application {
 
     public static void main(String[] args) {
